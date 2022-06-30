ee1089b1da125a964b4d178e4d93e59957fca068
NHC <nguyenhaichau1512@gmail.com>
2022-06-30 22:03:58 +0700

    first commit

---
+++ MovieTicketBooking_Client/build/generated/jax-wsCache/MovieTicketBook/movieticketbooking_client/GetMoviesInfoResponse.java
@@ -0,0 +1,62 @@
+
+package movieticketbooking_client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getMoviesInfoResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getMoviesInfoResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://MovieTicketModel/}movieInfo" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getMoviesInfoResponse", propOrder = {
+    "_return"
+})
+public class GetMoviesInfoResponse {
+
+    @XmlElement(name = "return")
+    protected MovieInfo _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link MovieInfo }
+     *     
+     */
+    public MovieInfo getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link MovieInfo }
+     *     
+     */
+    public void setReturn(MovieInfo value) {
+        this._return = value;
+    }
+
+}
